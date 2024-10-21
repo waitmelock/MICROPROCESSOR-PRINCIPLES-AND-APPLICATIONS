c5928ed4c3ac84c2fb895f9a39ea27ceef5c404b
Weiting-project <109543674+waitmelock@users.noreply.github.com>
2024-10-21 20:45:54 +0800

    Update bonus.asm

--- bonus.asm
+++ bonus.asm
@@ -6,24 +6,35 @@ PSECT mytext,local,class=CODE,reloc=2
 ;TRISB=sign of FSR1L
 ;TRISC=sign of FSR2L
 neg macro a3,sign
-    MOVLW b'11111111';if negtive
+    MOVLW 0xFF;if negtive
     XORWF a3,F
     INCF a3
     MOVLW 0x80
     MOVWF sign
-    endm 
+endm 
+    
+neg_unsign macro a3,sign
+    MOVLW 0xFF;if negtive
+    XORWF a3,F
+    INCF a3
+    MOVLW 0x80
+    MOVWF sign
+endm 
     
 _multi_signed:
 MOVFF WREG,FSR1L
-MOVFF 0x001,FSR2L
+MOVFF 0x01,FSR2L
+CLRF TRISB
+CLRF TRISC
+BTFSS FSR2L,7;4 bits
+GOTO sign
+neg FSR2L,TRISC
 
 BTFSS FSR1L,7
 GOTO sign
 neg FSR1L,TRISB
 
-BTFSS FSR2L,7
-GOTO sign
-neg FSR2L,TRISC
+
     
 sign:
 MOVF TRISC,w
@@ -31,13 +42,40 @@ XORWF TRISB,w
 MOVWF TRISA  
 
 MOVLW 0x01    
-MOVWF 0x000;mask
-function:
-0x00
-BTFSS FSR1L,0
-CLRF BSR
+MOVWF 0x00;mask
+MOVLW 0x00
+MOVWF 0x10;function i
 
-RLNFS
+;mul
+CLRF FSR0H
+MOVFF FSR1L,0x11;low bits
+function:
+RRNCF FSR2L 
+BTFSS FSR2L,0
+GOTO noadd
+MOVF 0x011,w
+ADDWF FSR0L,F
+MOVFF 0x11,0x12
+MOVFF 0x10,0x13
+
+DECFSZ 0x13
+RRNCF 0x12
+
+MOVF 0x12,w
+SUBWF FSR1L,w
+ADDWF FSR0H
     
+noadd:
+BCF 0x11,7    
+RLNCF 0x11    
+INCF 0x10
+MOVLW 0x03
+CPFSEQ 0x10
+GOTO function
+
+    
+    
+MOVFF FSR0L,0x01
+MOVFF FSR0H,0x02
     
 RETURN
