93f2828938edccbc6ed1e5c0df33a27d01603150
Weiting-project <109543674+waitmelock@users.noreply.github.com>
2024-11-21 00:46:48 +0800

    Update advance.asm 11/20

--- advance.asm
+++ advance.asm
@@ -55,35 +55,33 @@
 ; CONFIG7H
   CONFIG  EBTRB = OFF           ; Boot Block Table Read Protection bit (Boot block (000000-0007FFh) not protected from table reads executed in other blocks)
 
-    count EQU 0x14
-    org 0x00
+count EQU 0x14
+org 0x00
   
 goto Initial	
 
 ISR:				
     org 0x08			
-    BCF PIR1, TMR2IF        ; 離開前記得把TMR2IF清空 (清空flag bit)
-    ;INCF state
-
-	MOVLW 0x00
-	CPFSEQ count
-	GOTO judge0
-	GOTO s0
-judge0:
+    BCF PIR1, TMR2IF        ; ??????TMR2IF?? (??flag bit)
+    MOVLW 0x00
+    CPFSEQ count
+    GOTO judge0
+    GOTO s0
+    judge0:
 	MOVLW 0x0F
     	CPFSGT count
 	GOTO judge1
 	GOTO s16
-
-	judge1:
-    MOVLW 0x07
-    CPFSGT count
-    GOTO judge2
-    GOTO s3
+    judge1:
+	MOVLW 0x07
+	CPFSGT count
+	GOTO judge2
+	GOTO s3
     judge2:
-      MOVLW 0x03
-      CPFSGT count
-      GOTO s2
+	MOVLW 0x03
+	CPFSGT count
+	GOTO s1
+	GOTO s2
 
     s0:
 	CLRF LATA
@@ -94,8 +92,8 @@ judge0:
 	CLRF count
 	GOTO send
     s1:
-      MOVLW D'61'		
-      MOVWF PR2	
+	MOVLW D'61'		
+	MOVWF PR2	
 	INCF LATA
 	INCF count
     	GOTO send
@@ -122,16 +120,16 @@ Initial:
     CLRF LATA
     BSF RCON, IPEN
     BSF INTCON, GIE
-    BCF PIR1, TMR2IF		; 為了使用TIMER2，所以要設定好相關的TMR2IF、TMR2IE、TMR2IP。
+    BCF PIR1, TMR2IF		; ????TIMER2??????????TMR2IF?TMR2IE?TMR2IP?
     BSF IPR1, TMR2IP
     BSF PIE1 , TMR2IE
-    MOVLW b'11111111'	        ; 將Prescale與Postscale都設為1:16，意思是之後每256個週期才會將TIMER2+1
-    MOVWF T2CON		; 而由於TIMER本身會是以系統時脈/4所得到的時脈為主
-    MOVLW D'61'		; 因此每256 * 4 = 1024個cycles才會將TIMER2 + 1
-    MOVWF PR2			; 若目前時脈為250khz，想要Delay 0.5秒的話，代表每經過125000cycles需要觸發一次Interrupt
-				; 因此PR2應設為 125000 / 1024 = 122.0703125， 約等於122。
+    MOVLW b'11111111'	        ; ?Prescale?Postscale???1:16???????256??????TIMER2+1
+    MOVWF T2CON		; ???TIMER?????????/4????????
+    MOVLW D'61'		; ???256 * 4 = 1024?cycles???TIMER2 + 1
+    MOVWF PR2			; ??????250khz???Delay 0.5?????????125000cycles??????Interrupt
+				; ??PR2??? 125000 / 1024 = 122.0703125? ???122?
     MOVLW D'00100000'
-    MOVWF OSCCON	        ; 記得將系統時脈調整成250kHz
+    MOVWF OSCCON	        ; ??????????250kHz
 
 main:
     bra main
